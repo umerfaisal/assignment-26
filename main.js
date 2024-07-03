8a3fd335107138dd5f96857933e37cd9e78ac36d
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-07-03 18:01:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var alienColor = "green";
+if (alienColor === "green") {
+    console.log("You just earn five points");
+}
+else {
+    console.log("you just earn 10 points");
+}
+// 2nd version
+if (alienColor === "blue") {
+    console.log("I am come from if ");
+}
+else {
+    console.log("i am come from else");
+}
